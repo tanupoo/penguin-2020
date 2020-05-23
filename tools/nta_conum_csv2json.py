6e44c634298e0eafd9622b5b8ea783e210b9b188
tanupoo <github-shoichi@tanu.org>
2020-05-23 21:00:05 +0900

    enabled to read xlsx file directly.

--- tools/nta_conum_csv2json.py
+++ tools/nta_conum_csv2json.py
@@ -1,53 +1,43 @@
 #!/usr/bin/env python
 
-import sys
 import csv
 import json
-import re
+import xlrd
+import argparse
 
 """
 
 ## Usage
 
-nta_conum_csv2json.py (prefectures) (cities)
+nta_conum_to_json.py (prefs) (cities)
 
 e.g.
-nta_conum_csv2json.py nta-prefectures-20200512.csv nta-cities-20200512.csv > nta_conum.py
+nta_conum_to_json.py nta-prefs.xlsx nta-cities.xlsx > nta_conum.json
+
+## Input
+
+- 国税庁法人番号公表サイト
+- ホーム > 法人番号とは > 国の機関等一覧
+    https://www.houjin-bangou.nta.go.jp/setsumei/kuninokikanichiran.html
+- 普通地方公共団体
+- 都道府県(Excel形式)
+- 市町村(Excel形式)
+
+直接ダウンロードできないため、クリックして保存する。
+
+    #curl -o nta-prefs.xlsx https://www.houjin-bangou.nta.go.jp/setsumei/images/prefectures.xlsx
+    #curl -o nta-cities.xlsx https://www.houjin-bangou.nta.go.jp/setsumei/images/cities.xlsx
+    <html><head><title>Request Rejected</title></head><body>The requested URL was rejected. Please consult with your administrator.<br><br>Your support ID is: 15054605593445702901<br><br><a href='javascript:history.back();'>[Go Back]</a></body></html>
 
 ## Output
 
-co_num = {
+{
     "愛知県": "1000020230006",
     "愛知県名古屋市": "3000020231002",
     "愛知県豊橋市": "3000020232025",
     ...
 };
-
-## 自治体番号
-
-taken from https://www.soumu.go.jp/denshijiti/code.html
-
 """
-
-enable_compress = True
-enable_python = True
-
-skip_lines = 2 # for skipping the header.
-
-indent = None
-if enable_compress == False:
-    indent = 4
-
-csv_prefs = sys.argv[1]
-csv_cities = sys.argv[2]
-
-pref_dict = {}
-with open(csv_prefs) as fd:
-    it = csv.reader(fd, dialect=csv.excel)
-    for _ in range(skip_lines):
-        it.__next__()
-    for row in it:
-        pref_dict.update({ row[1]: row[0] })
 
 def find_pref(addr):
     for k in pref_dict.keys():
@@ -55,17 +45,52 @@ def find_pref(addr):
             return k
     raise ValueError(f"ERROR: {addr} is not in the pref list.")
 
+#
+# main
+#
+ap = argparse.ArgumentParser()
+ap.add_argument("prefs_file", help="CSV file of the prefectures.")
+ap.add_argument("cities_file", help="CSV file of the cities.")
+ap.add_argument("-i", action="store", dest="indent", type=int,
+                help="specify the number of columns for indent. e.g. -i 4")
+ap.add_argument("-s", action="store", dest="skip_lines", type=int, default=2,
+                help="""specify the number of lines at the header
+                in the file to skip.""")
+ap.add_argument("-x", action="store_true", dest="transx",
+                help="enable to make a dict for other program.")
+ap.add_argument("-d", action="store_true", dest="debug",
+                help="enable debug mode.")
+opt = ap.parse_args()
+
+# prefs
+xls_wb = xlrd.open_workbook(opt.prefs_file)
+if opt.debug:
+    print("sheets =", xls_wb.sheet_names())
+xls_sheet = xls_wb.sheet_by_index(0)
+
+rows = xls_sheet.get_rows()
+for _ in range(opt.skip_lines):
+    row = rows.__next__()
+
+pref_dict = {}
+for row in rows:
+    pref_dict.update({ row[1].value: row[0].value })
+
+# cities
 conum_dict = pref_dict.copy()
-with open(csv_cities) as fd:
-    it = csv.reader(fd, dialect=csv.excel)
-    for _ in range(skip_lines):
-        it.__next__()
-    for row in it:
-        pref = find_pref(row[2])
-        conum_dict.update({ "".join([pref,row[1]]): row[0] })
 
-if enable_python:
-    print("co_num_dict = ", end="")
+xls_wb = xlrd.open_workbook(opt.cities_file)
+if opt.debug:
+    print("sheets =", xls_wb.sheet_names())
+xls_sheet = xls_wb.sheet_by_index(0)
 
-print(json.dumps(conum_dict, indent=indent, ensure_ascii=False))
+rows = xls_sheet.get_rows()
+for _ in range(opt.skip_lines):
+    row = rows.__next__()
+
+for row in rows:
+    pref = find_pref(row[2].value)
+    conum_dict.update({ "".join([pref,row[1].value]): row[0].value })
+
+print(json.dumps(conum_dict, indent=opt.indent, ensure_ascii=False))
 
